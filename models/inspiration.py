c87cd17ea4cb4e756b4017ef47c88ec93a555f6e
Robert Dempsey <robertonrails@gmail.com>
2015-08-29 07:05:54 -0500

    Update measurements and models.
    Changed the measurements tracked to be scale weight, bf%, stomach and hips. Updated models to comply with Python standards and remove unnecessary imports.
    
    Signed-off-by: Robert Dempsey <robertonrails@gmail.com>

--- models/inspiration.py
+++ models/inspiration.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from flask import url_for
 from weighttracker import db
 
+
 class Inspiration(db.Document):
-  phrase = db.StringField(required=True, unique=True)
-  created_at = db.DateTimeField(default=datetime.now, required=True)
-  updated_at = db.DateTimeField(default=datetime.now, required=True)
+    phrase = db.StringField(required=True, unique=True)
+    created_at = db.DateTimeField(default=datetime.now, required=True)
+    updated_at = db.DateTimeField(default=datetime.now, required=True)
--- models/measurement.py
+++ models/measurement.py
@@ -1,14 +1,16 @@
 from datetime import datetime
-from flask import url_for
 from weighttracker import db
 
+
 class Measurement(db.Document):
-  date = db.StringField(required=True, unique=True, max_length=100)
-  weight = db.DecimalField()
-  body_fat_percentage = db.DecimalField()
-  suprailiac = db.DecimalField()
-  abdominal = db.DecimalField()
-  thigh = db.DecimalField()
-  measurement_time = db.DateTimeField(default=datetime.now, required=True)
-  created_at = db.DateTimeField(default=datetime.now, required=True)
-  updated_at = db.DateTimeField(default=datetime.now, required=True)
+    date = db.StringField(required=True, unique=True, max_length=100)
+    weight = db.DecimalField()
+    body_fat_percentage = db.DecimalField()
+    suprailiac = db.DecimalField()
+    abdominal = db.DecimalField()
+    thigh = db.DecimalField()
+    stomach = db.DecimalField()
+    hips = db.DecimalField()
+    measurement_time = db.DateTimeField(default=datetime.now, required=True)
+    created_at = db.DateTimeField(default=datetime.now, required=True)
+    updated_at = db.DateTimeField(default=datetime.now, required=True)
